8b86247d4983f90989fd5b56a5f27be3229dad48
Luis Emmanuel De Andrade Zamora <developer-three@outlook.com>
2019-02-10 02:13:16 -0500

    integracion angular

--- config/views.js
+++ config/views.js
@@ -36,6 +36,20 @@ module.exports.views = {
   *                                                                          *
   ***************************************************************************/
 
-  layout: 'layouts/layout'
+  layout: 'layouts/layout',
+
+    /****************************************************************************
+  *                                                                           *
+  * Partials are simply top-level snippets you can leverage to reuse template *
+  * for your server-side views. If you're using handlebars, you can take      *
+  * advantage of Sails' built-in `partials` support.                          *
+  *                                                                           *
+  * If `false` or empty partials will be located in the same folder as views. *
+  * Otherwise, if a string is specified, it will be interpreted as the        *
+  * relative path to your partial files from `views/` folder.                 *
+  *                                                                           *
+  ****************************************************************************/
+
+ partials: false
 
 };
